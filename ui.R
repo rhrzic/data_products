efa1f1ceddcd39801d2fc892468c75a651bd6244
rhrzic <rok.hrzic@gmail.com>
2016-04-03 22:23:05 +0200

    Added some comments

--- ui.R
+++ ui.R
@@ -10,11 +10,19 @@ library(shiny)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("How old are you in the world?"),
+  titlePanel("Where do you fit in?"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
+      h3('Hello, friend!'),
+      h4('Introduction'),
+      h6('This simple application is the result of the Coursera class on Data products. 
+         It aims to offer some perspective on the world population and highlight the incredible
+         potential young people have to shape the future of our world.'),
+      h4('Methodology'),
+      h6('The application then uses UN age data to calculate the percentage of people 
+          worldwide you are older than and displays your position on the World Age Histogram.'),
       sliderInput("age",
                   "How old are you?",
                   min = 1,
@@ -28,7 +36,8 @@ shinyUI(fluidPage(
 
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("agePlot")
+      plotOutput("agePlot"),
+      h6('Source of data: esa.un.org')
     )
   )
 ))
